e193bf6151d88d82e03b97983e5b361f3fde0bca
sarathtaj-eng <sarathtaj@gmail.com>
2026-06-25 03:43:03 +0400

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require("dotenv").config();
 
 const app = express();
 app.use(express.json());
-
+const bcrypt = require("bcrypt");
 const pool = new Pool({
     connectionString: process.env.DATABASE_URL,
     ssl: {
@@ -75,7 +75,61 @@ app.get("/",async(req,res)=>{
     });
 
 });
+// Register User
+app.post("/register", async (req, res) => {
 
+    try {
+
+        const { username, email, password } = req.body;
+
+        // Check empty fields
+        if (!username || !email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "All fields are required"
+            });
+        }
+
+        // Check if email already exists
+        const existingUser = await pool.query(
+            "SELECT * FROM users WHERE email=$1",
+            [email]
+        );
+
+        if (existingUser.rows.length > 0) {
+            return res.status(400).json({
+                success: false,
+                message: "Email already registered"
+            });
+        }
+
+        // Encrypt password
+        const passwordHash = await bcrypt.hash(password, 10);
+
+        // Save user
+        await pool.query(
+            `INSERT INTO users(username,email,password_hash)
+             VALUES($1,$2,$3)`,
+            [username, email, passwordHash]
+        );
+
+        res.json({
+            success: true,
+            message: "User registered successfully"
+        });
+
+    } catch (err) {
+
+        console.error(err);
+
+        res.status(500).json({
+            success: false,
+            message: "Server Error"
+        });
+
+    }
+
+});
 const PORT=process.env.PORT||3000;
 
 app.listen(PORT,()=>{
