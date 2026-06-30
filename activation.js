c1c5ec06e57a21195dcb924451b857dd1a5ea06e
sarathtaj-eng <sarathtaj@gmail.com>
2026-06-30 23:00:31 +0400

    Update activation.js

--- activation.js
+++ activation.js
@@ -15,8 +15,8 @@ const pool = new Pool({
 // =====================================
 // Generate Activation Code
 // =====================================
+router.get("/generate-activation", async (req, res) => {
 
-router.post("/generate-activation", async (req, res) => {
 
     try {
 
