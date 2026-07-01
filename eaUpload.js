15903a2b8c1829911c70628cd3e36c8c8477cd0f
sarathtaj-eng <sarathtaj@gmail.com>
2026-07-01 17:09:55 +0400

    Create eaUpload.js

---
+++ eaUpload.js
@@ -0,0 +1,70 @@
+const express = require("express");
+const multer = require("multer");
+const path = require("path");
+const adminAuth = require("./adminAuth");
+
+const router = express.Router();
+
+const storage = multer.diskStorage({
+
+    destination:function(req,file,cb){
+
+        cb(null,"uploads/");
+
+    },
+
+    filename:function(req,file,cb){
+
+        cb(
+
+            null,
+
+            "TradeGuardEA_" + Date.now() + path.extname(file.originalname)
+
+        );
+
+    }
+
+});
+
+const upload = multer({
+
+    storage:storage,
+
+    fileFilter:function(req,file,cb){
+
+        if(path.extname(file.originalname).toLowerCase() !== ".ex5"){
+
+            return cb(new Error("Only EX5 files allowed."));
+
+        }
+
+        cb(null,true);
+
+    }
+
+});
+
+router.post(
+
+    "/upload-ea",
+
+    adminAuth,
+
+    upload.single("ea"),
+
+    async(req,res)=>{
+
+        res.json({
+
+            success:true,
+
+            filename:req.file.filename
+
+        });
+
+    }
+
+);
+
+module.exports = router;
