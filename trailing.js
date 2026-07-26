6374cbb262e16bbd126ba0283eca8e3a4c7162cd
sarathtaj-eng <sarathtaj@gmail.com>
2026-07-26 17:55:58 +0400

    Create trailing.js

---
+++ trailing.js
@@ -0,0 +1,38 @@
+function enableTrailing(btn){
+
+    btn.className = "btn-blue";
+    btn.innerHTML = "⚙ Manage";
+
+    btn.onclick = function(){
+        openSettings(btn);
+    };
+
+    let row = btn.closest("tr");
+    let badge = row.querySelector(".inactive-badge");
+
+    if(badge){
+        badge.className = "active-badge";
+        badge.innerHTML = "ACTIVE";
+    }
+
+}
+
+function openSettings(btn){
+
+    document.getElementById("settingsModal").style.display = "block";
+
+}
+
+function closeSettings(){
+
+    document.getElementById("settingsModal").style.display = "none";
+
+}
+
+function disableTrailing(){
+
+    document.getElementById("settingsModal").style.display = "none";
+
+    alert("Trailing Disabled");
+
+}
